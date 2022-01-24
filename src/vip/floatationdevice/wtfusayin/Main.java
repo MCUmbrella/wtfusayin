304d9eda54001f241732da8e53e207985a7482b2
MCUmbrella <mcumbrella@protonmail.com>
2022-01-24 16:54:43 +0800

    revert useless changes
    i know but i refuse

--- src/vip/floatationdevice/wtfusayin/Main.java
+++ src/vip/floatationdevice/wtfusayin/Main.java
@@ -1,64 +1,52 @@
 package vip.floatationdevice.wtfusayin;
-
 import java.util.Scanner;
 
 import java.util.Random;
 import java.util.NoSuchElementException;
-
 public class Main
 {
-    final static Random r = new Random();
-    final static Scanner i = new Scanner(System.in);
+    final static Random r=new Random();
+    final static Scanner i=new Scanner(System.in);
     static int a;
-    public static StringBuilder pre = new StringBuilder();
-    public static StringBuilder post = new StringBuilder();
-
+    public static StringBuilder pre=new StringBuilder();
+    public static StringBuilder post=new StringBuilder();
+    
     public static void destroy(StringBuilder old, StringBuilder processed)
     {
-        while (old.length() != 0)
+        for(;old.length()!=0;)
         {
-            a = r.nextInt(old.length());
+            a=r.nextInt(old.length());
             processed.append(old.charAt(a));
-            old.delete(a, a + 1);
+            old.delete(a,a+1);
         }
     }
-
-    public static void main(String[] args)
+    
+    public static void main(String args[])
     {
-        if (args.length == 1 && args[0].equals("-g"))
-        {
-            //Graphical mode (GUI)
+        if(args.length==1&&args[0].equals("-g"))
+        {//Graphical mode (GUI)
             new GUI().setVisible(true);
         }
-        else if (args.length >= 1 && args[0].equals("-c"))
-        {
-            //Common mode (CLI)
+        else if(args.length>=1&&args[0].equals("-c"))
+        {//Common mode (CLI)
             try
             {
-                if (args.length == 1)
+                if(args.length==1)
                 {
-                    //TODO: Use ^D to end input in CLI
-                    while (pre.append(i.nextLine()).length() != 0)
+                    for(;pre.append(i.nextLine()).length()!=0;)//TODO: Use ^D to end input in CLI
                     {
                         post.setLength(0);
-                        destroy(pre, post);
+                        destroy(pre,post);
                         System.out.println(post.toString());
                     }
-                }
-                else
+                }else
                 {
-                    for (int i = 1; i != args.length; i++)
-                    {
-                        pre.append(args[i]).append(' ');
-                    }
-                    pre.delete(pre.length() - 1, pre.length());
-                    destroy(pre, post);
+                    for(int i=1;i!=args.length;i++){pre.append(args[i]).append(' ');}
+                    pre.delete(pre.length()-1,pre.length());
+                    destroy(pre,post);
                     System.out.println(post.toString());
                 }
-            }
-            catch (NoSuchElementException ignored)
-            {
-            }
+            }catch(NoSuchElementException e){}
         }
         else
         {
